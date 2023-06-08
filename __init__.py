7b2021804ac0971551872d26a5257ccc1500cdf4
PTrabajos <20190895@aloe.ulima.edu.pe>
2023-06-08 13:45:38 -0500

    first commit

---
+++ __init__.py
@@ -0,0 +1,94 @@
+#Código de Recibir WhatsApp y crear una respuesta con ChatGPT
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+#CUANDO RECIBAMOS LAS PETICIONES EN ESTA RUTA
+@app.route("/webhook/", methods=["POST", "GET"])
+def webhook_whatsapp():
+    #SI HAY DATOS RECIBIDOS VIA GET
+    if request.method == "GET":
+        #SI EL TOKEN ES IGUAL AL QUE RECIBIMOS
+        if request.args.get('hub.verify_token') == "PruebaUno":
+            #ESCRIBIMOS EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
+            return request.args.get('hub.challenge')
+        else:
+            #SI NO SON IGUALES RETORNAMOS UN MENSAJE DE ERROR
+          return "Error de autentificacion."
+    #RECIBIMOS TODOS LOS DATOS ENVIADO VIA JSON
+    data=request.get_json()
+    #EXTRAEMOS EL NUMERO DE TELEFONO Y EL MENSAJE
+    telefonoCliente=data['entry'][0]['changes'][0]['value']['messages'][0]['from']
+    #EXTRAEMOS EL TELEFONO DEL CLIENTE
+    mensaje=data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
+    #EXTRAEMOS EL ID DE WHATSAPP DEL ARRAY
+    idWA=data['entry'][0]['changes'][0]['value']['messages'][0]['id']
+    #EXTRAEMOS EL TIEMPO DE WHATSAPP DEL ARRAY
+    timestamp=data['entry'][0]['changes'][0]['value']['messages'][0]['timestamp']
+    #ESCRIBIMOS EL NUMERO DE TELEFONO Y EL MENSAJE EN EL ARCHIVO TEXTO
+    #SI HAY UN MENSAJE
+    if mensaje is not None:
+      
+        import openai
+        # Indica el API Key
+        openai.api_key = "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"
+        # Uso de ChapGPT en Python
+        model_engine = "text-davinci-003"
+        prompt = mensaje
+        completion = openai.Completion.create(engine=model_engine,
+                                            prompt=prompt,
+                                            max_tokens=1024,
+                                            n=1,
+                                            stop=None,
+                                            temperature=0.7)
+        respuesta=""
+        for choice in completion.choices:
+            respuesta=respuesta+choice.text
+            print(f"Response: %s" % choice.text)
+
+        respuesta=respuesta.replace("\\n","\\\n")
+        respuesta=respuesta.replace("\\","")
+        #ESCRIBIMOS LA RESPUESTA EN EL ARCHIVO TEXTO
+        f = open("texto.txt", "w")
+        f.write(respuesta)
+        f.close()
+        
+        #CONECTAMOS A LA BASE DE DATOS
+        import mysql.connector
+        mydb = mysql.connector.connect(
+          host = "mysql-testchatbot.alwaysdata.net",
+          user = "313959_chatgpt",
+          password = "MAPAO2708",
+          database='testchatbot_gptwa'
+        )
+        mycursor = mydb.cursor()
+        query="SELECT count(id) AS cantidad FROM registro WHERE id_wa='" + idWA + "';"
+        mycursor.execute(query)
+
+        cantidad, = mycursor.fetchone()
+        cantidad=str(cantidad)
+        cantidad=int(cantidad)
+        if cantidad==0 :
+            sql = ("INSERT INTO registro"+ 
+            "(mensaje_recibido,mensaje_enviado,id_wa      ,timestamp_wa   ,telefono_wa) VALUES "+
+            "('"+mensaje+"'   ,'"+respuesta+"','"+idWA+"' ,'"+timestamp+"','"+telefonoCliente+"');")
+            mycursor.execute(sql)
+            mydb.commit()
+            enviar(telefonoCliente,respuesta)
+        #RETORNAMOS EL STATUS EN UN JSON
+        return jsonify({"status": "success"}, 200)
+def enviar(telefonoRecibe,respuesta):
+  from heyoo import WhatsApp
+  #TOKEN DE ACCESO DE FACEBOOK
+  token='EAAWrF6YvccUBAP2AZBZCcZCEvoZAtd0jJQF7vMdgZC96LYZC0bgRXnQ4XFDrAsdbIPjWZAOegQfMv6wgDFmZCtb2abP8nig0XZAxeGI2qnK3J0ppZBYiZBXCI91WJBXfO5Iz6ov1ufDspiaNfjCQhNfZCG0M5IsJNZBmwiRm4MrukOcbukw6AXdkrQW8snaPox0Af2VWlelYZBJZBqEdQZDZD'
+  #IDENTIFICADOR DE NÚMERO DE TELÉFONO
+  idNumeroTeléfono='115458991551520'
+  #INICIALIZAMOS ENVIO DE MENSAJES
+  mensajeWa=WhatsApp(token,idNumeroTeléfono)
+  
+  #ENVIAMOS UN MENSAJE DE TEXTO
+  mensajeWa.send_message(respuesta,telefonoRecibe)
+        
+        
+
+#INICIAMSO FLASK
+if __name__ == "__main__":
+  app.run(debug=True)
